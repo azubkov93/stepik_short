62480f6bf96adb3939cdfaca9abed10dc323c8b1
azubkov93 <46124676+azubkov93@users.noreply.github.com>
2022-03-04 13:08:10 +0300

    2

--- conftest.py
+++ conftest.py
@@ -1,16 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--lang', action='store', default=None,
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption("--language", action="store", default=None,
                      help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_lang = request.config.getoption("lang")
-    fp = webdriver.FirefoxProfile()
-    fp.set_preference("intl.accept_languages", user_lang)
-    browser = webdriver.Firefox(firefox_profile=fp)
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    user_lang = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option("prefs", {"intl.accept_languages": user_lang})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_lang)
+        browser = webdriver.Firefox(firefox_profile=fp)
     return browser
--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ def test_basket_button(browser):
     try:
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
         browser.get(link)
-        time.sleep(10)
+        time.sleep(30)
         button = browser.find_elements_by_css_selector(".btn-add-to-basket")
         assert button is not []
     finally:
